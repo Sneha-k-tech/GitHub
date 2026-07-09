9758850606fe9e275d2d58c8dac2f95f926da32e
Sneha-k-tech <sneha@karmaasource.com>
2026-07-09 15:48:32 +0530

    added divi theme

--- wp-config.php
+++ wp-config.php
@@ -89,6 +89,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
+define('WP_ENVIRONMENT_TYPE', 'local');
 
 
 /* That's all, stop editing! Happy publishing. */
